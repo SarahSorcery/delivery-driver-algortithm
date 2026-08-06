354f272edd7a14e84c2830d9d4e04e9a281c8563
Sam Merrill <ladypug2002@gmail.com>
2026-08-06 00:23:27 -0400

    initial commit

---
+++ packageObject.py
@@ -0,0 +1,31 @@
+# Package Object
+
+class Package:
+    def __init__(self, package_id, address, city, state, zipcode, deadline, weight, status):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
+
+    def print_info(self):
+        print("ID: " +str(self.package_id))
+        print("Address: " + self.address)
+        print("City: " + self.city)
+        print("State: " + self.state)
+        print("Zipcode: " + self.zipcode)
+        print("Deadline: " + str(self.deadline))
+        print("Weight: " + self.weight)
+        print("Status: " + self.status)
+        print("********************************")
+# delivery ID
+# delivery address
+# delivery deadline
+# delivery city
+# delivery state
+# delivery zip code
+# package weight
+# delivery status
