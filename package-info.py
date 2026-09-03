1f845cad1b8efb49c1e75e3543e1db7ff56832b2
Sam Merrill <ladypug2002@gmail.com>
2026-09-03 03:40:08 -0400

    file cleaning

--- package-info.py
+++ package-info.py
@@ -1,32 +1,22 @@
 import csv
-from asyncio.windows_events import NULL
-
 import packageObject as pkg
 Pack = pkg.Package
 
-
-package_list = []
-
 def get_package_info(package_file_path):
+    loading_list = []
     with open(package_file_path) as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
         for line in reader:
             package_id, address, city, state, zipcode, deadline, weight, status = line
-            package_list.append(pkg.Package(package_id, address, city, state, zipcode, deadline, weight, status))
-
-#pkg.Package.print_info(package_list[22])
+            loading_list.append(pkg.Package(package_id, address, city, state, zipcode, deadline, weight, status))
+    return loading_list
 
 # populate list of packages from source CSV file
-get_package_info("WGUPS-package-file.csv")
-
-# print for testing purposes
-# for item in package_list:
-#     Pack.print_info(item)
-
+package_list = get_package_info("WGUPS-package-file.csv")
 
 ###################################################################################################
-# right now I have a list of Objects, which I need to assign to a hash table
+# assigning package_list items to hash table
 ###################
 
 # TESTING A HASH TABLE IMPLEMENTATION
@@ -63,11 +53,15 @@ def contains(value):
 
 
 # hash each package in list by id
-for package in package_list:
-    add_hash(package, hash_list)
-    #add(int(package.get_id()))
+# for package in package_list:
+#     add_hash(package, hash_list)
+#     #add(int(package.get_id()))
 
+def hash_packages(package_list):
+    for package in package_list:
+        add_hash(package, hash_list)
 
+hash_packages(package_list)
 
 #print out single pkg info by ID
 print("INFO FOR SINGLE PKG TESTING")
@@ -78,25 +72,15 @@ print(item_id)
 
 
 # # print hash list for testing
-for bucket in hash_list:
-    for item in bucket:
-        print(item)
-        Pack.print_info(item)
+def print_hash_table():
+    for bucket in hash_list:
+        for item in bucket:
+            print(item)
+            Pack.print_info(item)
+
+print_hash_table()
 
 ###################################################################################################
-# ****** excess old code from above: ******
-# we need to grab the ID, hash that value, and store the entire piece of data into the "bucket"
-# def add_hash(package, hash_list):
-#     index = hash_function(int(package.get_id()))
-#     hash_list[index].append(package)
-#     #return index
-# def remove_hash(package, hash_list):
-#     index = hash_function(int(package.get_id()))
-#     hash_list[index].remove(package)
-#
-# def update_hash(package, hash_list):
-#     index = hash_function(int(package.get_id()))
-#     hash_list[index].append(package)
 
 distance_table = []
 
@@ -110,49 +94,48 @@ def get_distance_info(distance_file_path):
                 address_distance.append(data)
             distance_table.append(address_distance)
 
-
-
-
 get_distance_info("WGUPS-distance-table.csv")
-#
-# for location in distance_table:
-#     for distance in location:
-#         print(distance)
-#     print("********************************")
+########
 location_distance_values = []
 for location in distance_table:
     distance_values = location[1:] # take only number values after addresses
     location_distance_values.append(distance_values) # this is more efficient than a 2d for loop, which I initially had here, which created a list of values from location 2-end, but this is way better
 
-
 for i in location_distance_values:
     print(i)
     print("********************************")
 
-
-
+################################
+def print_statuses():
+    for bucket in hash_list:
+        for package in bucket:
+            if Pack.get_status(package) != "":
+                print(str(Pack.get_id(package)) + "  |  " + Pack.get_status(package))
+print_statuses()
 ################################
 
 # selected_id = input("Enter which package ID: " )
 
-
 # enter an ID and search the HASH LIST for the package
-
-
-
 
 ### TO DOS
     # [ ] assign route lists
             #  group by status constraints? : below, make into function if needed
 
-for bucket in hash_list:
-    for package in bucket:
-        if Pack.get_status(package) != "":
-            print(Pack.get_id(package))
-            print(Pack.get_status(package))
-
-
-
 
 ## this was working i thought, now it 's not
 # print(contains(27))
+
+# ****** excess old code from above: ******
+# we need to grab the ID, hash that value, and store the entire piece of data into the "bucket"
+# def add_hash(package, hash_list):
+#     index = hash_function(int(package.get_id()))
+#     hash_list[index].append(package)
+#     #return index
+# def remove_hash(package, hash_list):
+#     index = hash_function(int(package.get_id()))
+#     hash_list[index].remove(package)
+#
+# def update_hash(package, hash_list):
+#     index = hash_function(int(package.get_id()))
+#     hash_list[index].append(package)
